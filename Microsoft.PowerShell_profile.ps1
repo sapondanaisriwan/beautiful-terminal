fc1632985da8a2c86cdfe228370e7765a068925f
Anton Ryadovoy <a.ryadovoy@gmail.com>
2024-12-21 00:54:07 +0300

    deleted_profile

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-oh-my-posh init pwsh --config 'C:\Users\aryad\AppData\Local\Programs\oh-my-posh\themes\nordcustom.omp.json' | Invoke-Expression
-import-Module -Name Terminal-Icons
-Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-set-psreadlineoption -PredictionViewStyle ListView
-                         
